bd5977736926ef73f9a71d401d11d36a83b5e611
AndreHG <andreshg9203@gmail.com>
2024-11-09 13:29:56 -0300

    restorage deploy

--- database/init.sql
+++ database/init.sql
@@ -1,12 +1,13 @@
 --table for tasks
 CREATE TABLE task(
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255) UNIQUE NOT NULL,
-    description TEXT 
+    title VARCHAR(255) NOT NULL,
+    description TEXT, 
+    user_id INTEGER REFERENCES users(id)
 );
 
 --alterando la tabla para agregar relacion
-ALTER TABLE task ADD COLUMN user_id INTEGER REFERENCES users(id);
+--ALTER TABLE task ADD COLUMN user_id INTEGER REFERENCES users(id);
 
 --table for users
 CREATE TABLE users(
@@ -14,9 +15,10 @@ CREATE TABLE users(
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
+    gravatar VARCHAR(255), 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 --alterando la tabla de users para agregar link de imagen perfil
-ALTER TABLE users ADD COLUMN gravatar VARCHAR(255);
+--ALTER TABLE users ADD COLUMN gravatar VARCHAR(255);
